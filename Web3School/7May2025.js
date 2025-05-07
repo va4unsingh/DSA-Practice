28530f8775a21b4d4820cf0b40481f190a6fc231
Varun <varunsingh7004@gmail.com>
2025-05-07 22:23:33 +0530

    bit bit clear

--- Web3School/7May2025.js
+++ Web3School/7May2025.js
@@ -1,73 +1,84 @@
-let number1 = 0;
-let number2 = 1;
+// let number1 = 0;
+// let number2 = 1;
 
-for (let i = 0; i < 18; i++) {
-  let newFibo = number1 + number2;
-  //   console.log(newFibo);
-  number1 = number2;
-  number2 = newFibo;
-}
+// for (let i = 0; i < 18; i++) {
+//   let newFibo = number1 + number2;
+//   //   console.log(newFibo);
+//   number1 = number2;
+//   number2 = newFibo;
+// }
 
-// recursion
+// // recursion
+
+// // console.log(0);
+// // console.log(1);
+
+// count = 2;
+
+// function fibonacci(prev1, prev2) {
+//   if (count <= 19) {
+//     const newFibo = prev1 + prev2;
+//     // console.log(newFibo);
+//     count++;
+
+//     fibonacci(newFibo, prev1);
+//   }
+// }
+
+// fibonacci(1, 0);
+
+// // Finding The nth Fibonacci Number Using Recursion
+
+// function F(n) {
+//   if (n <= 1) {
+//     return n;
+//   } else {
+//     return F(n - 1) + F(n - 2);
+//   }
+// }
+
+// console.log(F(19));
+
+// // pratice by own self
 
 // console.log(0);
 // console.log(1);
 
-count = 2;
+// let count = 2;
 
-function fibonacci(prev1, prev2) {
-  if (count <= 19) {
-    const newFibo = prev1 + prev2;
-    // console.log(newFibo);
-    count++;
+// function fibonacci(prev1, prev2) {
+//   if (count <= 19) {
+//     // your logic here
 
-    fibonacci(newFibo, prev1);
-  }
-}
+//     // 1. Calculate newFibo
+//     let newFibo = prev1 + prev2;
 
-fibonacci(1, 0);
+//     // 2. Print it
+//     // console.log(newFibo);
 
-// Finding The nth Fibonacci Number Using Recursion
+//     // 3. Update prev1 and prev2
+//     prev2 = prev1;
+//     prev1 = newFibo;
 
-function F(n) {
-  if (n <= 1) {
-    return n;
-  } else {
-    return F(n - 1) + F(n - 2);
-  }
-}
+//     // 4. Increase count
+//     count++;
 
-console.log(F(19));
+//     // 5. Call fibonacci again with new values
+//     fibonacci(prev1, prev2);
+//   } else {
+//     return;
+//   }
+// }
 
-// pratice by own self
+// fibonacci(1, 0);
 
-console.log(0);
-console.log(1);
+// ... spread operator confusion clear
 
-let count = 2;
+let a = [1, 2, 3, 4];
+let b = [80, 90, 100];
 
-function fibonacci(prev1, prev2) {
-  if (count <= 19) {
-    // your logic here
+//console.log(a, b);  // output : [ 1, 2, 3, 4 ] [ 80, 90, 100 ]
 
-    // 1. Calculate newFibo
-    let newFibo = prev1 + prev2;
+//console.log(...a, ...b); // output : 1 2 3 4 80 90 100
 
-    // 2. Print it
-    console.log(newFibo);
-
-    // 3. Update prev1 and prev2
-    prev2 = prev1;
-    prev1 = newFibo;
-
-    // 4. Increase count
-    count++;
-
-    // 5. Call fibonacci again with new values
-    fibonacci(prev1, prev2);
-  } else {
-    return;
-  }
-}
-
-fibonacci(1, 0);
+console.log([...a, ...b]); // output : [1, 2, 3, 4, 80, 90, 100]
