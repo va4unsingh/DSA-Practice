fc81ecac61f181a03b22f593a13479e9b57b8ff7
Varun <varunsingh7004@gmail.com>
2025-05-07 17:26:43 +0530

    recursive  code

--- Web3School/7May2025.js
+++ Web3School/7May2025.js
@@ -3,7 +3,26 @@ let number2 = 1;
 
 for (let i = 0; i < 18; i++) {
   let newFibo = number1 + number2;
-  console.log(newFibo);
+  //   console.log(newFibo);
   number1 = number2;
   number2 = newFibo;
 }
+
+// recursion
+
+console.log(0);
+console.log(1);
+
+count = 2;
+
+function fibonacci(prev1, prev2) {
+  if (count <= 19) {
+    const newFibo = prev1 + prev2;
+    console.log(newFibo);
+    count++;
+
+    fibonacci(newFibo, prev1);
+  }
+}
+
+fibonacci(1, 0);
