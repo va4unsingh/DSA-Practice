a8fac1ec53b45fd6c53e3c24ff80c54d27167bd7
Varun <varunsingh7004@gmail.com>
2025-05-10 04:03:57 +0530

    combine strings

---
+++ GPT BASIC DSA START/10May.js
@@ -0,0 +1,23 @@
+// function sumAll(...args) {
+//   return args.reduce((sum, num) => sum + num, 0);
+// }
+
+// console.log(sumAll(1, 2,10,10,10));
+
+// 2
+
+// let combineStrings = (...args) => {
+//   let a = "";
+//   for (let i = 0; i < args.length; i++) {
+//     a += args[i];
+//     if (i < args.length - 1) {
+//       a += " ";
+//     }
+//   }
+//   return a;
+// };
+
+// let vader = combineStrings("vader", "bhai");
+
+// console.log(vader);
+
