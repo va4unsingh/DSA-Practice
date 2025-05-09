5181dfd7e837fa13aaa5b8501823e4746cf13421
Varun <varunsingh7004@gmail.com>
2025-05-10 05:01:22 +0530

    reverse an array

--- GPT BASIC DSA START/10May.js
+++ GPT BASIC DSA START/10May.js
@@ -36,15 +36,71 @@
 
 // another one question
 
-let arr = [2, 5, 1, 9, 3];
-let largest = arr[0];
-let index = 0;
+// let arr = [2, 5, 1, 9, 3];
+// let largest = arr[0];
+// let index = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > largest) {
-    largest = arr[i];
-    index = i;
-  }
-}
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > largest) {
+//     largest = arr[i];
+//     index = i;
+//   }
+// }
 
-console.log(`${largest}, ${index}`);
+// console.log(`${largest}, ${index}`);
+
+// another one
+
+// let arr = [12, 35, 1, 10, 34, 1];
+
+// let findSecondLargest = (arr) => {
+//   let largest = arr[0];
+//   let secondLargest = arr[0];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const num = arr[i];
+//     if (num > largest) {
+//       secondLargest = largest;
+//       largest = num;
+//     } else if (num > secondLargest && num !== largest) {
+//       secondLargest = num;
+//     }
+//   }
+//   return secondLargest;
+// };
+
+// console.log(findSecondLargest(arr));
+
+// another one
+
+// let arr = [2, 4, 1, 7, 10];
+
+// function largestNumber(arr) {
+//   let largest = arr[0];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//       largest = arr[i];
+//     }
+//   }
+//   return largest;
+// }
+
+// console.log(largestNumber(arr));
+
+// sum an array
+
+// let arr = [1, 2, 3, 4, 5];
+
+// function sumOfArray(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+// console.log(sumOfArray(arr));
+
+// reverse an array
+
