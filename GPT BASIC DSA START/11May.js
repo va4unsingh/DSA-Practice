d689d631ce1fb994778e3731873973e11a6a0522
Varun <varunsingh7004@gmail.com>
2025-05-11 16:50:57 +0530

    done

--- GPT BASIC DSA START/11May.js
+++ GPT BASIC DSA START/11May.js
@@ -1,7 +1,41 @@
-let arr = [1, [2, 3], [4, [5, 6]], 7];
+// let arr = [10, [20, 30], [40, [50, 60]], 70];
 
-function fn(n) {
-  return n + 1;
+// function fn(n, i) {
+//   return n * i;
+// }
+
+// function transform(arr, fn) {
+//   let res = [];
+//   let index = 0;
+
+//   function flat(num) {
+//     for (let i = 0; i < num.length; i++) {
+//       if (Array.isArray(num[i])) {
+//         flat(num[i]);
+//       } else {
+//         res.push(fn(num[i], index));
+//         index++;
+//       }
+//     }
+//   }
+
+//   flat(arr);
+//   return res;
+// }
+
+// console.log(transform(arr, fn));
+
+// by auto
+
+let arr = [10, [20, 30], [40, [50, 60]], 70];
+
+function fn(n, i) {
+  return n * i;
 }
 
-function tran
+function vader(arr, fn) {
+  let flat = arr.flat(Infinity);
+  return flat.map((value, index) => fn(value, index));
+}
+
+console.log(vader(arr, fn));
