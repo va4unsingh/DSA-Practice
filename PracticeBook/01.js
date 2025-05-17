c90b67f540a190bc97abb5189dbbd9507d8477ca
Varun Singh <varunsingh7004@gmail.com>
2025-05-17 20:06:02 +0530

    Practice

--- PracticeBook/01.js
+++ PracticeBook/01.js
@@ -1,36 +1,36 @@
-function countdown(i) {
-  console.log(i);
-  countdown(i - 1);
-}
+// function countdown(i) {
+//   console.log(i);
+//   countdown(i - 1);
+// }
 
 // ⚠️ Warning: this will crash quickly with a “Maximum call stack size exceeded” error
-countdown(10);
+// countdown(10);
 
-let i = 5;
-while (true) {
-  console.log(i);
-  i--;
-}
+// let i = 5;
+// while (true) {
+//   console.log(i);
+//   i--;
+// }
 
-function countdown(i) {
-  console.log(i);
-  if (i <= 1) return; // base case
-  countdown(i - 1); // recursive call
-}
+// function countdown(i) {
+//   console.log(i);
+//   if (i <= 1) return; // base case
+//   countdown(i - 1); // recursive call
+// }
 
 // Start the countdown
-countdown(3);
+// countdown(3);
 
 // Output:
 // 3
 // 2
 // 1
 
-function fact(x) {
-  if (x === 1) return 1;
-  else {
-    return x * fact(x - 1);
-  }
-}
+// function fact(x) {
+//   if (x === 1) return 1;
+//   else {
+//     return x * fact(x - 1);
+//   }
+// }
 
-console.log(fact(5));
+// console.log(fact(5));
