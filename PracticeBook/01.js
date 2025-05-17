07ca55c67df93c200bfd93afffded339d2656503
Varun Singh <varunsingh7004@gmail.com>
2025-05-17 20:45:32 +0530

    self

--- PracticeBook/01.js
+++ PracticeBook/01.js
@@ -34,3 +34,15 @@
 // }
 
 // console.log(fact(5));
+
+let arr = [1, 2, 3, 4];
+
+function vader(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+console.log(vader(arr));
