1e3f47311b2022994783c49235b32f2219848d39
Varun Singh <varunsingh7004@gmail.com>
2025-05-17 15:50:28 +0530

    02 whole done

--- grokking_algorithms/02_selection_sort/02_recursive_selection_sort.js
+++ grokking_algorithms/02_selection_sort/02_recursive_selection_sort.js
@@ -27,3 +27,28 @@
 // console.log("Source array - ", sourceArray); // [5, 3, 6, 2, 10]
 // console.log("New sorted array - ", sourtedArray); // [2, 3, 5, 6, 10]
 
+// old js
+
+const findSmallest = (arr, smallest = arr[0], smallestIndex = 0, i = 1) => {
+  if (arr.length <= i) return smallestIndex;
+  const curr = arr[i];
+
+  if (curr < smallest) {
+    smallest = curr;
+    smallestIndex = i;
+  }
+  return findSmallest(arr, smallest, smallestIndex, i + 1);
+};
+
+const selectionSort = (arr, result = []) => {
+  if (arr.length > 0) {
+    const smallestIndex = findSmallest(arr);
+    const [smallest] = arr.splice(smallestIndex, 1);
+    result.push(smallest);
+    return selectionSort(arr, result);
+  }
+  return result;
+};
+
+const arr = [5, 3, 6, 2, 10];
+console.log(selectionSort(arr));
