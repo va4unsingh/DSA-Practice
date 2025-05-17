1fa4a3624ffa83ff9bdccc1daeef92733a10f224
Varun Singh <varunsingh7004@gmail.com>
2025-05-17 21:42:17 +0530

    self

--- PracticeBook/01.js
+++ PracticeBook/01.js
@@ -70,16 +70,15 @@
 
 // console.log(sum(array));
 
-// now using recursion 
+// now using recursion
 
 let array = [1, 2, 3, 4, 5];
 
-// function sum(array) {
-//   let vader = 0;
-//   for (let i = 0; i < array.length; i++) {
-//     vader += array[i];
-//   }
-//   return vader;
-// }
+function sum(array) {
+  if (array.length === 0) return 0;
+  else {
+    return array[0] + sum(array.slice(1));
+  }
+}
 
-// console.log(sum(array));
+console.log(sum(array));
