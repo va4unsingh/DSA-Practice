853526c641a9b8bcaa2715955cd5d7f8a1a3ff50
Varun Singh <varunsingh7004@gmail.com>
2025-05-19 21:03:12 +0530

    Example

--- self practice/01.js
+++ self practice/01.js
@@ -41,3 +41,106 @@
 // }
 
 // console.log(hasDuplicate(nums));
+
+// brute force
+// let s = "anagram";
+// let t = "nagaram";
+
+// function isAnagram(s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+
+//   let sSort = s.split("").sort().join();
+//   let tSort = t.split("").sort().join();
+//   return sSort == tSort;
+// }
+// console.log(isAnagram(s, t));
+
+// hash maps
+
+// let s = "anagram";
+// let t = "nagaram";
+
+// function isAnagram(s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+
+//   const countS = {};
+//   const countT = {};
+//   for (let i = 0; i < s.length; i++) {
+//     countS[s[i]] = (countS[s[i]] || 0) + 1;
+//     countT[t[i]] = (countT[t[i]] || 0) + 1;
+//   }
+//   for (const key in countS) {
+//     if (countS[key] !== countT[key]) {
+//       return false;
+//     }
+//     return true;
+//   }
+// }
+// console.log(isAnagram(s, t));
+
+// using hash arrays
+
+// let s = "anagram";
+// let t = "nagaram";
+
+// function isAnagram(s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+
+//   const count = new Array(26).fill(0);
+//   for (let i = 0; i < s.length; i++) {
+//     count[s.charCodeAt(i) - "a".charCodeAt(0)]++;
+//     count[t.charCodeAt(i) - "a".charCodeAt(0)]--;
+//   }
+//   return count.every((val) => val === 0);
+// }
+
+// console.log(isAnagram(s, t));
+
+// hash using map again
+
+// let s = "anagram";
+// let t = "nagaram";
+
+// function isAnagram(s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+//   let countS = {};
+//   let countT = {};
+//   for (let i = 0; i < s.length; i++) {
+//     countS[s[i]] = (countS[s[i]] || 0) + 1;
+//     countT[t[i]] = (countT[t[i]] || 0) + 1;
+//   }
+//   for (const key in countS) {
+//     if (countS[key] !== countT[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isAnagram(s, t));
+
+// hash array again
+let s = "anagram";
+let t = "nagaram";
+
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const count = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - "a".charCodeAt(0)]++;
+    count[t.charCodeAt(i) - "a".charCodeAt(0)]--;
+  }
+  return count.every((val) => val === 0);
+}
+
+console.log(isAnagram(s, t));
