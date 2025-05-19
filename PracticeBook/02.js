659d797f6879fcb05ce9ae318993f69ee2a2c9d3
Varun Singh <varunsingh7004@gmail.com>
2025-05-19 08:08:37 +0530

    Quick short

---
+++ PracticeBook/02.js
@@ -0,0 +1,17 @@
+let array = [10, 5, 2, 3];
+
+function quicksort(array) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    let pivot = array[0];
+
+    const less = array.slice(1).filter((item) => item <= pivot);
+
+    const greater = array.slice(1).filter((item) => item > pivot);
+
+    return [...quicksort(less), pivot, ...quicksort(greater)];
+  }
+}
+
+console.log(quicksort(array)); // [2,3,5,10]
