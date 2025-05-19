d8c3f09170836329f07a0f31204a1dfcce1a068a
Varun Singh <varunsingh7004@gmail.com>
2025-05-19 09:03:23 +0530

    Example

--- PracticeBook/02.js
+++ PracticeBook/02.js
@@ -1,17 +1,34 @@
-let array = [10, 5, 2, 3];
+// let array = [10, 5, 2, 3];
 
-function quicksort(array) {
-  if (array.length < 2) {
-    return array;
-  } else {
-    let pivot = array[0];
+// function quicksort(array) {
+//   if (array.length < 2) {
+//     return array;
+//   } else {
+//     let pivot = array[0];
 
-    const less = array.slice(1).filter((item) => item <= pivot);
+//     const less = array.slice(1).filter((item) => item <= pivot);
 
-    const greater = array.slice(1).filter((item) => item > pivot);
+//     const greater = array.slice(1).filter((item) => item > pivot);
 
-    return [...quicksort(less), pivot, ...quicksort(greater)];
-  }
-}
+//     return [...quicksort(less), pivot, ...quicksort(greater)];
+//   }
+// }
 
-console.log(quicksort(array)); // [2,3,5,10]
+// const sorted = quicksort(array); // [2,3,5,10]
+
+// console.log(sorted.join(", "));
+
+// let myList = ["apple", "banana", "cherry"];
+
+// function printItems(myList) {
+//   for (const item of myList) {
+//     console.log(item);
+//   }
+// }
+// printItems(myList);
+
+// or
+// function printItems(myList) {
+//   myList.forEach((item) => console.log(item));
+// }
+// printItems(myList);
